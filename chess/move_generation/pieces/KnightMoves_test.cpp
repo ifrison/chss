193e719b3f1dad00081d965562bac70fc06e6a3c
Inaki Frison Pujol <inaki.frison@king.com>
2025-01-09 17:34:33 +0100

    Add unit tests to KnightMoves.

--- chess/move_generation/pieces/KnightMoves_test.cpp
+++ chess/move_generation/pieces/KnightMoves_test.cpp
@@ -4,13 +4,9 @@
 
 #include <test_utils/TestUtils.h>
 
-
-#include "chess/DebugUtils.h"
-#include <iostream>
-
 namespace {
 
-template <typename Generator>
+template<typename Generator>
 constexpr std::size_t GeneratorSize(const Generator& generator) {
 	std::size_t size = 0;
 	for ([[maybe_unused]] const auto& _ : generator) {
@@ -30,10 +26,53 @@ constexpr std::array<T, S> GeneratorToArray(const Generator& generator) {
 	return result;
 }
 
-}
+} // namespace
 
 TEST_CASE("KnightMoves", "Unobstructed") {
 	constexpr auto state = chss::fen::Parse("8/8/8/3N4/8/8/8/8 w - - 0 1");
+	constexpr auto generator = chss::move_generation::KnightPseudoLegalMoves(state, chss::positions::D5);
+	constexpr auto size = GeneratorSize(generator);
+	constexpr auto array = GeneratorToArray<chss::Move, size>(generator);
+	constexpr auto expectedResult = std::array<chss::Move, 8>{
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::C3, .promotionType = std::nullopt},
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::E3, .promotionType = std::nullopt},
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::B4, .promotionType = std::nullopt},
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::F4, .promotionType = std::nullopt},
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::B6, .promotionType = std::nullopt},
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::F6, .promotionType = std::nullopt},
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::C7, .promotionType = std::nullopt},
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::E7, .promotionType = std::nullopt},
+	};
+	STATIC_REQUIRE(array == expectedResult);
+}
+
+TEST_CASE("KnightMoves", "EdgeOfBoard") {
+	constexpr auto state = chss::fen::Parse("8/N7/8/8/8/8/8/8 w - - 0 1");
+	constexpr auto generator = chss::move_generation::KnightPseudoLegalMoves(state, chss::positions::A7);
+	constexpr auto size = GeneratorSize(generator);
+	constexpr auto array = GeneratorToArray<chss::Move, size>(generator);
+	constexpr auto expectedResult = std::array<chss::Move, 3>{
+		chss::Move{.from = chss::positions::A7, .to = chss::positions::B5, .promotionType = std::nullopt},
+		chss::Move{.from = chss::positions::A7, .to = chss::positions::C6, .promotionType = std::nullopt},
+		chss::Move{.from = chss::positions::A7, .to = chss::positions::C8, .promotionType = std::nullopt},
+	};
+	STATIC_REQUIRE(array == expectedResult);
+}
+
+TEST_CASE("KnightMoves", "ObstructedByFriendlyPiece") {
+	constexpr auto state = chss::fen::Parse("8/2K1Q3/5R2/3N4/5B2/2P1N3/8/8 w - - 0 1");
+	constexpr auto generator = chss::move_generation::KnightPseudoLegalMoves(state, chss::positions::D5);
+	constexpr auto size = GeneratorSize(generator);
+	constexpr auto array = GeneratorToArray<chss::Move, size>(generator);
+	constexpr auto expectedResult = std::array<chss::Move, 2>{
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::B4, .promotionType = std::nullopt},
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::B6, .promotionType = std::nullopt},
+	};
+	STATIC_REQUIRE(array == expectedResult);
+}
+
+TEST_CASE("KnightMoves", "CaptureEnemyPiece") {
+	constexpr auto state = chss::fen::Parse("8/2k1q3/5r2/3N4/5b2/2p1n3/8/8 w - - 0 1");
 	constexpr auto generator = chss::move_generation::KnightPseudoLegalMoves(state, chss::positions::D5);
 	constexpr auto size = GeneratorSize(generator);
 	constexpr auto array = GeneratorToArray<chss::Move, size>(generator);
