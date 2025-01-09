1a59e093dd39a0c5dd24c6811cc8958f1432d2e5
Inaki Frison Pujol <inaki.frison@king.com>
2025-01-10 00:36:22 +0100

    Improve KnightMoves.

--- chess/move_generation/pieces/KnightMoves.h
+++ chess/move_generation/pieces/KnightMoves.h
@@ -3,7 +3,7 @@
 #include "chess/representation/Move.h"
 #include "chess/representation/State.h"
 
-namespace {
+namespace detail {
 
 constexpr auto kMoveOffsets = std::array<matrix::Direction2D, 8>{
 	matrix::Direction2D{.deltaY = -2, .deltaX = -1},
@@ -16,78 +16,80 @@ constexpr auto kMoveOffsets = std::array<matrix::Direction2D, 8>{
 	matrix::Direction2D{.deltaY = +2, .deltaX = +1},
 };
 
-constexpr std::size_t FindNextMoveOffsetIndex(const chss::State& state, const chss::Position& knightPosition, std::size_t startIndex) {
-	for (std::size_t i = startIndex; i < kMoveOffsets.size(); ++i) {
+constexpr std::size_t FindNextMoveOffsetIndex(
+	const chss::State& state,
+	const chss::Position& knightPosition,
+	const std::size_t startIndex) {
+	std::size_t i = startIndex;
+	while (i < kMoveOffsets.size()) {
 		const auto position = knightPosition + kMoveOffsets[i];
-		if (state.board.IsInside(position)) {
-			const auto& pieceOpt = state.board.At(position);
-			if (!pieceOpt.has_value() || pieceOpt.value().color == chss::InverseColor(state.activeColor)) {
-				return i;
-			}
+		if (state.board.IsInside(position) &&
+			(!state.board.At(position).has_value() || state.board.At(position).value().color != state.activeColor)) {
+			return i;
 		}
+		++i;
 	}
-	return kMoveOffsets.size();
+	return i;
 }
 
-}
+class Generator {
+public:
+	class Sentinel {};
 
-namespace chss::move_generation {
-
-[[nodiscard]] constexpr auto KnightPseudoLegalMoves(const State& state, const Position& knightPosition) {
-	class Generator {
+	class Iterator {
 	public:
-		class Sentinel {};
-
-		class Iterator {
-		public:
-			constexpr explicit Iterator(const State& state, const Position& knightPosition)
+		constexpr explicit Iterator(const chss::State& state, const chss::Position& knightPosition)
 			: mState(state)
 			, mKnightPosition(knightPosition)
 			, mMoveOffsetIndex(FindNextMoveOffsetIndex(state, knightPosition, 0)) {}
 
-			[[nodiscard]] constexpr Move operator*() const {
-				assert(mMoveOffsetIndex < kMoveOffsets.size());
-				return Move{.from = mKnightPosition, .to = mKnightPosition + kMoveOffsets[mMoveOffsetIndex], .promotionType = std::nullopt};
-			}
-
-			constexpr Iterator& operator++() {
-				assert(mMoveOffsetIndex < kMoveOffsets.size());
-				mMoveOffsetIndex = FindNextMoveOffsetIndex(mState, mKnightPosition, mMoveOffsetIndex + 1);
-				return *this;
-			}
-
-			[[nodiscard]] constexpr bool operator==(const Sentinel&) const {
-				return mMoveOffsetIndex == kMoveOffsets.size();
-			}
-
-			[[nodiscard]] constexpr bool operator!=(const Sentinel&) const {
-				return mMoveOffsetIndex != kMoveOffsets.size();
-			}
-
-		private:
-			const State& mState;
-			const Position mKnightPosition;
-			std::size_t mMoveOffsetIndex;
-		};
-
-		constexpr explicit Generator(const State& state, const Position& knightPosition)
-			: mState(state)
-			, mKnightPosition(knightPosition) {}
-
-		[[nodiscard]] constexpr Iterator begin() const {
-			return Iterator(mState, mKnightPosition);
+		[[nodiscard]] constexpr chss::Move operator*() const {
+			assert(mMoveOffsetIndex < kMoveOffsets.size());
+			return chss::Move{
+				.from = mKnightPosition,
+				.to = mKnightPosition + kMoveOffsets[mMoveOffsetIndex],
+				.promotionType = std::nullopt};
 		}
 
-		[[nodiscard]] constexpr Sentinel end() const {
-			return Sentinel{};
+		constexpr Iterator& operator++() {
+			assert(mMoveOffsetIndex < kMoveOffsets.size());
+			mMoveOffsetIndex = FindNextMoveOffsetIndex(mState, mKnightPosition, mMoveOffsetIndex + 1);
+			return *this;
+		}
+
+		[[nodiscard]] constexpr bool operator==(const Sentinel&) const {
+			return mMoveOffsetIndex == kMoveOffsets.size();
+		}
+
+		[[nodiscard]] constexpr bool operator!=(const Sentinel&) const {
+			return mMoveOffsetIndex != kMoveOffsets.size();
 		}
 
 	private:
-		State mState;
-		Position mKnightPosition;
+		const chss::State& mState;
+		const chss::Position mKnightPosition;
+		std::size_t mMoveOffsetIndex;
 	};
 
-	return Generator(state, knightPosition);
+	constexpr explicit Generator(const chss::State& state, const chss::Position& knightPosition)
+		: mState(state)
+		, mKnightPosition(knightPosition) {}
+
+	[[nodiscard]] constexpr Iterator begin() const { return Iterator(mState, mKnightPosition); }
+
+	[[nodiscard]] constexpr Sentinel end() const { return Sentinel{}; }
+
+private:
+	chss::State mState;
+	chss::Position mKnightPosition;
+};
+
+} // namespace detail
+
+namespace chss::move_generation {
+
+[[nodiscard]] constexpr auto KnightPseudoLegalMoves(const State& state, const Position& knightPosition) {
+	return detail::Generator(state, knightPosition);
 }
 
-}
+} // namespace chss::move_generation
