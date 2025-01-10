50b2b2390fba7f559479e5d7cef4405910497d26
Inaki Frison Pujol <inaki.frison@king.com>
2025-01-10 11:24:14 +0100

    PawnMoves draft.

---
+++ chess/move_generation/pieces/PawnMoves.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include "chess/representation/Move.h"
+#include "chess/representation/State.h"
+
+namespace detail {
+
+constexpr auto kPawnMoveOffsets = std::array<matrix::Direction2D, 4>{
+	matrix::Direction2D{.deltaY = +1, .deltaX = 0},
+	matrix::Direction2D{.deltaY = +1, .deltaX = -1},
+	matrix::Direction2D{.deltaY = +1, .deltaX = +1},
+	matrix::Direction2D{.deltaY = +2, .deltaX = 0},
+};
+
+constexpr std::size_t FindNextPawnMoveOffsetIndex(
+	const chss::State& state,
+	const chss::Position& pawnPosition,
+	const std::size_t startIndex) {
+	std::size_t i = startIndex;
+	while (i < kPawnMoveOffsets.size()) {
+		const auto position = state.activeColor == chss::Color::White ? pawnPosition + kPawnMoveOffsets[i] : pawnPosition - kPawnMoveOffsets[i];
+		switch (i) {
+		case 0: // Advance
+			if (state.board.IsInside(position) && !state.board.At(position).has_value()) {
+				return i;
+			}
+			break;
+		case 1: // Capture left
+		case 2: // Capture right
+			if (state.board.IsInside(position) &&
+				state.board.At(position).has_value() && state.board.At(position).value().color != state.activeColor) {
+				return i;
+			}
+			break;
+		case 3: // Double Advance
+			if ((pawnPosition.y == 1 && state.activeColor == chss::Color::White && !state.board.At(chss::Position{.y = 2, .x = pawnPosition.x}).has_value() && !state.board.At(chss::Position{.y = 3, .x = pawnPosition.x}).has_value()) ||
+				(pawnPosition.y == 6 && state.activeColor == chss::Color::White && !state.board.At(chss::Position{.y = 5, .x = pawnPosition.x}).has_value() && !state.board.At(chss::Position{.y = 4, .x = pawnPosition.x}).has_value())) {
+				return i;
+			}
+			break;
+		}
+		++i;
+	}
+	return i;
+}
+
+class Generator {
+public:
+	class Sentinel {};
+
+	class Iterator {
+	public:
+		constexpr explicit Iterator(const chss::State& state, const chss::Position& pawnPosition)
+			: mState(state)
+			, mPawnPosition(pawnPosition)
+			, mMoveOffsetIndex(FindNextPawnMoveOffsetIndex(state, pawnPosition, 0)) {}
+
+		[[nodiscard]] constexpr chss::Move operator*() const {
+			assert(mMoveOffsetIndex < kPawnMoveOffsets.size());
+			return chss::Move{
+				.from = mPawnPosition,
+				.to = mState.activeColor == chss::Color::White ? mPawnPosition + kPawnMoveOffsets[mMoveOffsetIndex] : mPawnPosition - kPawnMoveOffsets[mMoveOffsetIndex],
+				.promotionType = std::nullopt};
+		}
+
+		constexpr Iterator& operator++() {
+			assert(mMoveOffsetIndex < kPawnMoveOffsets.size());
+			mMoveOffsetIndex = FindNextPawnMoveOffsetIndex(mState, mPawnPosition, mMoveOffsetIndex + 1);
+			return *this;
+		}
+
+		[[nodiscard]] constexpr bool operator==(const Sentinel&) const {
+			return mMoveOffsetIndex == kPawnMoveOffsets.size();
+		}
+
+		[[nodiscard]] constexpr bool operator!=(const Sentinel&) const {
+			return mMoveOffsetIndex != kPawnMoveOffsets.size();
+		}
+
+	private:
+		const chss::State& mState;
+		const chss::Position mPawnPosition;
+		std::size_t mMoveOffsetIndex;
+	};
+
+	constexpr explicit Generator(const chss::State& state, const chss::Position& pawnPosition)
+		: mState(state)
+		, mPawnPosition(pawnPosition) {}
+
+	[[nodiscard]] constexpr Iterator begin() const { return Iterator(mState, mPawnPosition); }
+
+	[[nodiscard]] constexpr Sentinel end() const { return Sentinel{}; }
+
+private:
+	chss::State mState;
+	chss::Position mPawnPosition;
+};
+
+} // namespace detail
+
+namespace chss::move_generation {
+
+[[nodiscard]] constexpr auto PawnPseudoLegalMoves(const State& state, const Position& pawnPosition) {
+	return detail::Generator(state, pawnPosition);
+}
+
+} // namespace chss::move_generation
---
+++ chess/move_generation/pieces/PawnMoves_test.cpp
@@ -0,0 +1,131 @@
+#include "PawnMoves.h"
+
+#include "chess/fen/Fen.h"
+
+#include <test_utils/TestUtils.h>
+
+namespace {
+
+template<typename Generator>
+constexpr std::size_t GeneratorSize(const Generator& generator) {
+	std::size_t size = 0;
+	for ([[maybe_unused]] const auto& _ : generator) {
+		++size;
+	}
+	return size;
+}
+
+template<typename T, std::size_t S, typename Generator>
+constexpr std::array<T, S> GeneratorToArray(const Generator& generator) {
+	auto result = std::array<T, S>();
+	auto it = generator.begin();
+	for (std::size_t i = 0; i < S; ++i) {
+		result[i] = *it;
+		++it;
+	}
+	return result;
+}
+
+} // namespace
+
+// Advance
+TEST_CASE("PawnMoves", "Advance_White") {
+	constexpr auto state = chss::fen::Parse("8/8/8/3P4/8/8/8/8 w - - 0 1");
+	constexpr auto generator = chss::move_generation::PawnPseudoLegalMoves(state, chss::positions::D5);
+	constexpr auto size = GeneratorSize(generator);
+	constexpr auto array = GeneratorToArray<chss::Move, size>(generator);
+	constexpr auto expectedResult = std::array<chss::Move, 1>{
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::D6, .promotionType = std::nullopt},
+	};
+	STATIC_REQUIRE(array == expectedResult);
+}
+
+TEST_CASE("PawnMoves", "Advance_Black") {
+	constexpr auto state = chss::fen::Parse("8/8/8/3p4/8/8/8/8 b - - 0 1");
+	constexpr auto generator = chss::move_generation::PawnPseudoLegalMoves(state, chss::positions::D5);
+	constexpr auto size = GeneratorSize(generator);
+	constexpr auto array = GeneratorToArray<chss::Move, size>(generator);
+	constexpr auto expectedResult = std::array<chss::Move, 1>{
+		chss::Move{.from = chss::positions::D5, .to = chss::positions::D4, .promotionType = std::nullopt},
+	};
+	STATIC_REQUIRE(array == expectedResult);
+}
+
+TEST_CASE("PawnMoves", "Advance_BlockedByOtherPiece_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "Advance_BlockedByOtherPiece_Black") {
+	// TODO
+}
+
+TEST_CASE("PawnMoves", "Advance_BlockedByEndOfBoard_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "Advance_BlockedByEndOfBoard_Black") {
+	// TODO
+}
+
+TEST_CASE("PawnMoves", "Advance_Promotion_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "Advance_Promotion_Black") {
+	// TODO
+}
+
+// DoubleAdvance
+TEST_CASE("PawnMoves", "DoubleAdvance_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "DoubleAdvance_Black") {
+	// TODO
+}
+
+TEST_CASE("PawnMoves", "DoubleAdvance_BlockedShortByOtherPiece_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "DoubleAdvance_BlockedShortByOtherPiece_Black") {
+	// TODO
+}
+
+TEST_CASE("PawnMoves", "DoubleAdvance_BlockedLongByOtherPiece_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "DoubleAdvance_BlockedLongByOtherPiece_Black") {
+	// TODO
+}
+
+// Capture
+TEST_CASE("PawnMoves", "Capture_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "Capture_Black") {
+	// TODO
+}
+
+TEST_CASE("PawnMoves", "Capture_WhileOnLeftEdge_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "Capture_WhileOnLeftEdge_Black") {
+	// TODO
+}
+
+TEST_CASE("PawnMoves", "Capture_WhileOnRightEdge_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "Capture_WhileOnRightEdge_Black") {
+	// TODO
+}
+
+TEST_CASE("PawnMoves", "DontCaptureAtDistanceTwo_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "DontCaptureAtDistanceTwo_Black") {
+	// TODO
+}
+
+TEST_CASE("PawnMoves", "CaptureEnPassant_White") {
+	// TODO
+}
+TEST_CASE("PawnMoves", "CaptureEnPassant_Black") {
+	// TODO
+}
