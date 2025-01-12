cf968782cc0e300f163955692b1334c904359a60
Inaki Frison Pujol <inaki.frison@king.com>
2025-01-12 18:03:21 +0100

    Constexpr MinMax.

--- chess/MinMax.h
+++ chess/MinMax.h
@@ -1,16 +1,109 @@
 #pragma once
 
+#include "evaluation/Evaluation.h"
+#include "move_generation/LegalMoves.h"
+#include "representation/Move.h"
+#include "representation/State.h"
+
 #include <atomic>
 
-namespace chss {
+namespace detail {
 
-struct Move;
-struct State;
+[[nodiscard]] constexpr int Search0(const chss::State& state, int depth, const std::atomic_flag& stop) {
+	if (depth == 0) {
+		return chss::evaluation::Evaluate(state.board);
+	}
+	int result =
+		state.activeColor == chss::Color::White ? std::numeric_limits<int>::min() : std::numeric_limits<int>::max();
+	for (const auto move : chss::move_generation::LegalMoves(state)) {
+		if constexpr (!std::is_constant_evaluated()) {
+			if (stop.test()) {
+				break;
+			}
+		}
+		const auto newState = chss::move_generation::MakeMove(state, move);
+		const auto score = Search0(newState, depth - 1, stop);
+		if (state.activeColor == chss::Color::White) {
+			result = std::max(result, score);
+		} else {
+			result = std::min(result, score);
+		}
+	}
+	return result;
+}
 
-namespace search {
+[[nodiscard]] constexpr std::tuple<int, chss::Move> Search1(
+	const chss::State& state,
+	int depth,
+	const std::atomic_flag& stop) {
+	assert(depth > 0);
+	auto resultScore = state.activeColor == chss::Color::White ? std::numeric_limits<int>::min() : std::numeric_limits<int>::max();
+	auto resultMove = chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}};
+	for (const auto move : chss::move_generation::LegalMoves(state)) {
+		if constexpr (!std::is_constant_evaluated()) {
+			if (stop.test()) {
+				break;
+			}
+		}
+		const auto newState = chss::move_generation::MakeMove(state, move);
+		const auto score = Search0(newState, depth - 1, stop);
+		if (state.activeColor == chss::Color::White) {
+			if (score > resultScore) {
+				resultScore = score;
+				resultMove = move;
+			}
+		} else {
+			if (score < resultScore) {
+				resultScore = score;
+				resultMove = move;
+			}
+		}
+	}
+	return std::tuple<int, chss::Move>(resultScore, resultMove);
+}
 
-[[nodiscard]] std::pair<Move, Move> Search(const State& state, int depth, const std::atomic_flag& stop);
+[[nodiscard]] constexpr std::tuple<int, chss::Move, chss::Move> Search2(
+	const chss::State& state,
+	int depth,
+	const std::atomic_flag& stop) {
+	assert(depth > 1);
+	auto resultScore =
+		state.activeColor == chss::Color::White ? std::numeric_limits<int>::min() : std::numeric_limits<int>::max();
+	auto resultMove = chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}};
+	auto resultPonderMove =
+		chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}};
+	for (const auto move : chss::move_generation::LegalMoves(state)) {
+		if constexpr (!std::is_constant_evaluated()) {
+			if (stop.test()) {
+				break;
+			}
+		}
+		const auto newState = chss::move_generation::MakeMove(state, move);
+		const auto [score, ponderMove] = Search1(newState, depth - 1, stop);
+		if (state.activeColor == chss::Color::White) {
+			if (score > resultScore) {
+				resultScore = score;
+				resultMove = move;
+				resultPonderMove = ponderMove;
+			}
+		} else {
+			if (score < resultScore) {
+				resultScore = score;
+				resultMove = move;
+				resultPonderMove = ponderMove;
+			}
+		}
+	}
+	return std::tuple<int, chss::Move, chss::Move>(resultScore, resultMove, resultPonderMove);
+}
 
-} // namespace search
+} // namespace detail
 
-} // namespace chss
+namespace chss::search {
+
+[[nodiscard]] constexpr std::pair<Move, Move> Search(const State& state, int depth, const std::atomic_flag& stop) {
+	const auto [score, move, ponderMove] = detail::Search2(state, depth, stop);
+	return std::pair<Move, Move>(move, ponderMove);
+}
+
+} // namespace chss::search
--- chess/MinMax_test.cpp
+++ chess/MinMax_test.cpp
@@ -4,16 +4,34 @@
 #include "fen/Fen.h"
 #include "representation/Move.h"
 
+#include <test_utils/TestUtils.h>
+
 #include <gtest/gtest.h>
 
-TEST(Search, MateInOne) {
+TEST_CASE("Search", "MateInOne") {
 	auto stop = std::atomic_flag(false);
-	EXPECT_EQ(chss::search::Search(chss::fen::Parse("2k5/7R/2K5/8/8/8/8/8 w - - 0 1"), 2, stop), (std::pair<chss::Move, chss::Move>(chss::Move{.from = chss::positions::H7, .to = chss::positions::H8}, chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}})));
-	EXPECT_EQ(chss::search::Search(chss::fen::Parse("8/8/8/7q/8/2k5/8/2K5 b - - 0 1"), 2, stop), (std::pair<chss::Move, chss::Move>(chss::Move{.from = chss::positions::H5, .to = chss::positions::H1}, chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}})));
+	STATIC_REQUIRE(
+		chss::search::Search(chss::fen::Parse("2k5/7R/2K5/8/8/8/8/8 w - - 0 1"), 2, stop) ==
+		(std::pair<chss::Move, chss::Move>(
+			chss::Move{.from = chss::positions::H7, .to = chss::positions::H8},
+			chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}})));
+	STATIC_REQUIRE(
+		chss::search::Search(chss::fen::Parse("8/8/8/7q/8/2k5/8/2K5 b - - 0 1"), 2, stop) ==
+		(std::pair<chss::Move, chss::Move>(
+			chss::Move{.from = chss::positions::H5, .to = chss::positions::H1},
+			chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}})));
 }
 
-TEST(Search, MateInTwo) {
+TEST_CASE("Search", "MateInTwo") {
 	auto stop = std::atomic_flag(false);
-	EXPECT_EQ(chss::search::Search(chss::fen::Parse("7K/8/8/8/6R1/7R/1k6/8 w - - 0 1"), 4, stop), (std::pair<chss::Move, chss::Move>(chss::Move{.from = chss::positions::G4, .to = chss::positions::G2}, chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}})));
-	EXPECT_EQ(chss::search::Search(chss::fen::Parse("4K2b/8/4pk2/8/7N/6Q1/8/8 w - - 0 1"), 4, stop), (std::pair<chss::Move, chss::Move>(chss::Move{.from = chss::positions::E8, .to = chss::positions::F8}, chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}})));
+	REQUIRE(
+		chss::search::Search(chss::fen::Parse("7K/8/8/8/6R1/7R/1k6/8 w - - 0 1"), 4, stop) ==
+		(std::pair<chss::Move, chss::Move>(
+			chss::Move{.from = chss::positions::G4, .to = chss::positions::G2},
+			chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}})));
+	REQUIRE(
+		chss::search::Search(chss::fen::Parse("4K2b/8/4pk2/8/7N/6Q1/8/8 w - - 0 1"), 4, stop) ==
+		(std::pair<chss::Move, chss::Move>(
+			chss::Move{.from = chss::positions::E8, .to = chss::positions::F8},
+			chss::Move{.from = chss::Position{.y = -1, .x = -1}, .to = chss::Position{.y = -1, .x = -1}})));
 }
